749f48dcf5f6c6ae04d502f21bfaf73e045c5992
superinflation <103957277+superinflation@users.noreply.github.com>
2022-05-03 12:41:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+function amongusModRun111(){
   var url = "https://i.pinimg.com/originals/4d/73/f0/4d73f0cec466f102a91e336db64cc7f5.jpg";
   document.querySelector("title").innerHTML = "Ribbit"; 
   var images = document.querySelectorAll("img"); 
@@ -22,4 +22,5 @@
       meta[i].content = url;
     }
   }
+}
 

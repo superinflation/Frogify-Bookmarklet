50d1720de38ec0a8be06e0fae1d13f7c7bfbb2f3
superinflation <103957277+superinflation@users.noreply.github.com>
2022-05-04 09:01:28 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,14 @@
-alert("aaaa");
+(function() {
+  function start(){
+    alert("aaaa");
+  }
+   var original = window.Frogify;
 
+  var self = (window.Frogify = {
+    add: add,
+    noConflict: function() {
+      window.Frogify = original;
+      return self;
+    }
+  });
+})();

3b8296c17d2bfb69b1e138bd8e97638eca870b22
superinflation <103957277+superinflation@users.noreply.github.com>
2022-05-04 09:08:48 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,1 @@
-(function() {
-  function start(){
-    alert("aaaa");
-  }
-   var original = window.Frogify;
-
-  var self = (window.Frogify = {
-    add: start,
-    noConflict: function() {
-      window.Frogify = original;
-      return self;
-    }
-  });
-})();
+alert("aaaa");

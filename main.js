df20935d9a6c7196f9be6608c48eaa8c5d35318a
superinflation <103957277+superinflation@users.noreply.github.com>
2022-05-06 15:18:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+alert("aaaaaaaaa");
 var url = "https://i.pinimg.com/originals/4d/73/f0/4d73f0cec466f102a91e336db64cc7f5.jpg";
 document.querySelector("title").innerHTML = "Ribbit";
 var images = document.querySelectorAll("img");
